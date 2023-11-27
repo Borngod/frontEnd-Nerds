74354b66e021f1c8fe2c3e3142250ae830c385fa
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2023-11-27 19:34:54 +0000

    Fix: Set Boilerplate for the frontend

---
+++ frontEND/E-Commerce/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "import", "jsx-a11y"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
